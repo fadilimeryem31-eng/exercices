caeabf530536862e74a46ea6cb96306d0114b68c
fadilimeryem31-eng <fadilimeryem31@gmail.com>
2025-09-02 13:41:46 +0100

    la distance en yards

---
+++ day01/challenge3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float Yards , Km ;
+    printf("entrer la distance en km:");
+    scanf("%f",&Km);
+    
+    Yards = Km * 1093.61;
+    printf("la distance en yards est %f",Yards);
+    return 0;
+}
