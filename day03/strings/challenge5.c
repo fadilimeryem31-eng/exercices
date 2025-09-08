bdb18b0f0cf16e9d2bea101d328d2ec398dbbfbc
fadilimeryem31-eng <fadilimeryem31@gmail.com>
2025-09-08 12:27:29 +0100

    inverse de chaine

---
+++ day03/strings/challenge5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+    char str[20];
+    
+    
+    
+    printf("entrer les elements:");
+    scanf("%s",str);
+    
+   printf(" la chaine a inverse est %s",strrev(str));
+    return 0;
+}
