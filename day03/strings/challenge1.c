0d9f5bd3e1fadeb580fcd905a8af2cbd67832897
fadilimeryem31-eng <fadilimeryem31@gmail.com>
2025-09-08 10:03:06 +0100

    affichage des Affichage d'une Chaîne

---
+++ day03/strings/challenge1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+    char str[20];
+    printf("entrer les elements:");
+    scanf("%s",str);
+    
+    printf("afficher: %s",str);
+    
+    
+
+    return 0;
+}
